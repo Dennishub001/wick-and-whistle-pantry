c99853b8c0a001eb6599cea56e4d4db16ae5ced1
juanita mumbi <mumbijuanita@gmail.com>
2025-04-25 20:05:15 +0300

    completed styling and all

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeCard from './Components/RecipeCard';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:3000/recipes')
+      .then((res) => res.json())
+      .then((data) => setRecipes(data))
+      .catch((err) => console.error('Error fetching recipes:', err));
+  }, []);
 
   return (
-    <>
-       
-    </>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="recipe-container"> 
+                {recipes.map((recipe) => (
+                  <RecipeCard key={recipe.id} recipe={recipe} />
+                ))}
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
